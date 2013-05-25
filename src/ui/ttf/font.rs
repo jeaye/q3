90b944bd772c8e4d164f4b0f5d5ec20b851017eb
Jeaye <jeaye@arrownext.com>
2013-05-24 21:45:08 -0700

    #9 Added back in the new ttf code

---
+++ src/ui/ttf/font.rs
@@ -0,0 +1,194 @@
+/*
+    Copyright 2013 Jesse 'Jeaye' Wilkerson
+    See licensing in LICENSE file, or at:
+        http://www.opensource.org/licenses/BSD-3-Clause
+
+    File: gl/ttf/font.rs
+    Author: Jesse 'Jeaye' Wilkerson
+    Description:
+      TrueType font atlas.
+*/
+
+use std::hashmap::HashMap;
+use std::libc::{ c_uint };
+use math::*;
+use self::glyph::Glyph;
+
+#[path = "../../gl/mod.rs"]
+mod gl;
+#[path = "../../gl/util.rs"]
+mod util;
+#[path = "../../gl/check.rs"]
+mod check;
+
+mod glyph;
+
+#[path = "./ft.rs"]
+mod ft;
+
+struct Font
+{
+  file: ~str,
+  library: ft::Library,
+  face: ft::Face,
+  texture_atlas: gl::GLuint,
+  atlas_dimensions: Vec2i,
+  glyphs: HashMap<u8, Glyph>,
+  height: i32,
+}
+
+impl Font /* TODO: Check macro for Freetype. */
+{
+  pub fn new(filename: &str, size: i32) -> Font
+  {
+    let mut font = Font
+    {
+      file: filename.to_owned(),
+      library: ptr::null(),
+      face: ptr::null(),
+      texture_atlas: 0,
+      atlas_dimensions: Vec2i::zero(),
+      glyphs: HashMap::new::<u8, Glyph>(),
+      height: 0,
+    };
+
+    unsafe
+    {
+      ft::FT_Init_FreeType(&font.library);
+
+      do str::as_c_str(filename) |c_str|
+      {
+        if ft::FT_New_Face(font.library, c_str, 0, &font.face) != 0
+        { fail!(~"Failed to create TTF face."); }
+      }
+   
+      ft::FT_Set_Pixel_Sizes(font.face, 0, size as c_uint);
+
+      let ft_glyph = (*font.face).glyph;
+      let max_width = 1024;
+      let mut row_width = 0, row_height = 0;
+
+      let chars: ~str = ~" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^abcdefghijklmnopqrstuvwxyz{|}~";
+
+      for chars.each |curr|
+      {
+        if ft::FT_Load_Char(font.face, curr as u32, ft::LOAD_RENDER) != 0
+        { loop; }
+
+        /* If we've exhausted the width for this row, add another. */
+        if row_width + (*ft_glyph).bitmap.width + 1 >= max_width
+        {
+          font.atlas_dimensions.x = 
+            if font.atlas_dimensions.x > row_width /* TODO: Max? */
+            { font.atlas_dimensions.x }
+            else
+            { row_width };
+          font.atlas_dimensions.y += row_height;
+          row_width = 0; row_height = 0;
+        }
+
+        let mut glyph = Glyph::new(); /* TODO: Need to be f32 here? Messy. */
+        glyph.advance.x = ((*ft_glyph).advance.x >> 6) as f32;
+        glyph.advance.y = ((*ft_glyph).advance.y >> 6) as f32;
+        glyph.dimensions.x = (*ft_glyph).bitmap.width as f32;
+        glyph.dimensions.y = (*ft_glyph).bitmap.rows as f32;
+        glyph.offset.x = (*ft_glyph).bitmap_left as f32;
+        glyph.offset.y = (*ft_glyph).bitmap_top as f32;
+        glyph.buffer = vec::from_buf( (*ft_glyph).bitmap.buffer,
+                                      (glyph.dimensions.x * glyph.dimensions.y) as uint);
+
+        row_width += (glyph.dimensions.x + 1.0) as i32;
+        row_height = /* TODO: Max? */
+          if row_height > (*ft_glyph).bitmap.rows
+          { row_height }
+          else
+          { (*ft_glyph).bitmap.rows };
+        font.height = /* TODO: Max? */
+          if font.height > row_height
+          { font.height }
+          else
+          { row_height };
+
+        font.glyphs.insert(curr, glyph);
+      }
+
+      font.atlas_dimensions.x = next_power_of_2( /* TODO: Max? */
+                                          if font.atlas_dimensions.x > row_width
+                                          { font.atlas_dimensions.x }
+                                          else
+                                          { row_width });
+      font.atlas_dimensions.y = next_power_of_2(font.atlas_dimensions.y + row_height);
+
+      /* We're using 1 byte alignment buffering. */
+      check!(gl::pixel_store_i(gl::UNPACK_ALIGNMENT, 1));
+      
+      check!(gl::active_texture(gl::TEXTURE0));
+      font.texture_atlas = check!(gl::gen_textures(1))[0];
+      check!(gl::bind_texture(gl::TEXTURE_2D, font.texture_atlas));
+      check!(gl::tex_image_2d(gl::TEXTURE_2D, 0, gl::RGB as gl::GLint,
+                              font.atlas_dimensions.x, font.atlas_dimensions.y,
+                              0, gl::RED, gl::UNSIGNED_BYTE, None));
+
+      /* Clamp to the edge to avoid artifacts when scaling. */
+      check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32));
+      check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32));
+
+      /* Linear filtering usually looks best for text. */
+      check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32));
+      check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32));
+
+      /* Copy all glyphs into the texture atlas. */
+      let mut offset = Vec2i::zero();
+      row_height = 0;
+      for chars.each |curr|
+      {
+        let glyph = match font.glyphs.find_mut(&(curr as u8))
+        {
+          Some(g) => g,
+          None => fail!(fmt!("Invalid char (%?) in font %?", curr, filename))
+        };
+
+        if offset.x + (glyph.dimensions.x as i32) + 1 >= max_width
+        {
+          offset.y += row_height;
+          row_height = 0; offset.x = 0;
+        }
+
+        { /* temp has a short scope. */
+          let temp: &[u8] = glyph.buffer;
+          check!(gl::tex_sub_image_2d(
+                      gl::TEXTURE_2D, 0, offset.x, offset.y,
+                      glyph.dimensions.x as i32, glyph.dimensions.y as i32,
+                      gl::RED, gl::UNSIGNED_BYTE, Some(temp)));
+        }
+
+        /* Calculate the position in the texture. */
+        glyph.tex.x = (offset.x as f32 / (font.atlas_dimensions.x as f32));
+        glyph.tex.y = (offset.y as f32 / (font.atlas_dimensions.y as f32));
+
+        offset.x += glyph.dimensions.x as i32;
+        row_height = /* TODO: Max? */
+                if row_height > glyph.dimensions.y as i32
+                { row_height }
+                else
+                { glyph.dimensions.y as i32 };
+      }
+    }
+
+    /* Reset the state. */
+    check!(gl::pixel_store_i(gl::UNPACK_ALIGNMENT, 4));
+
+    assert!(font.height > 0);
+
+    font
+  }
+}
+impl Drop for Font
+{
+  fn finalize(&self)
+  {
+    unsafe
+    { ft::FT_Done_FreeType(self.library); }
+  }
+}
+
---
+++ src/ui/ttf/renderer.rs
@@ -0,0 +1,147 @@
+/*
+    Copyright 2013 Jesse 'Jeaye' Wilkerson
+    See licensing in LICENSE file, or at:
+        http://www.opensource.org/licenses/BSD-3-Clause
+
+    File: gl/ttf/renderer.rs
+    Author: Jesse 'Jeaye' Wilkerson
+    Description:
+      A TTF font renderer.
+*/
+
+use gl::shader::{ Shader, Shader_Builder };
+use gl::camera::Camera;
+use super::Font;
+use math::{ Vec2f, Mat4x4 };
+
+#[path = "../../gl/mod.rs"]
+mod gl;
+#[path = "../../gl/util.rs"]
+mod util;
+#[path = "../../gl/check.rs"]
+mod check;
+
+struct Renderer
+{
+  vao: gl::GLuint,
+  vbo: gl::GLuint,
+  shader: @Shader,
+  proj_loc: gl::GLint,
+}
+
+impl Renderer
+{
+  pub fn new() -> Renderer
+  {
+    let mut renderer = Renderer
+    {
+        vao: 0,
+        vbo: 0,
+        shader: Shader_Builder::new_with_files("data/shaders/text.vert", "data/shaders/text.frag"),
+        proj_loc: 0,
+    };
+    renderer.proj_loc = renderer.shader.get_uniform_location("proj");
+    let tex_loc = renderer.shader.get_uniform_location("tex0"); 
+    renderer.shader.bind();
+    renderer.shader.update_uniform_i32(tex_loc, 0);
+
+    renderer.vao = check!(gl::gen_vertex_arrays(1))[0];
+    check!(gl::bind_vertex_array(renderer.vao));
+    renderer.vbo = check!(gl::gen_buffers(1))[0];
+    check!(gl::bind_buffer(gl::ARRAY_BUFFER, renderer.vbo));
+    let data: ~[u8] = ~[];
+    check!(gl::buffer_data(gl::ARRAY_BUFFER, data, gl::STREAM_DRAW));
+    check!(gl::enable_vertex_attrib_array(0));
+
+    renderer
+  }
+
+  #[inline(always)]
+  pub fn begin(&mut self, camera: &Camera)
+  {
+    check!(gl::disable(gl::DEPTH_TEST));
+
+    check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32));
+    check!(gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32));
+
+    /* Enable transparency. */
+    check!(gl::enable(gl::BLEND));
+    check!(gl::blend_func(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA));
+
+    self.shader.bind();
+    let proj =  Mat4x4::new_orthographic(0.0, camera.window_size.x as f32, camera.window_size.y as f32, 0.0,  1.0, 100.0);
+    self.shader.update_uniform_mat(self.proj_loc, &proj);
+  }
+
+  #[inline(always)]
+  pub fn end(&mut self)
+  {
+    check!(gl::enable(gl::DEPTH_TEST));
+    check!(gl::disable(gl::BLEND));
+  }
+
+  pub fn render(&mut self, text: &str, pos: Vec2f, font: &Font)
+  {
+    check!(gl::active_texture(gl::TEXTURE0));
+    check!(gl::bind_texture(gl::TEXTURE_2D, font.texture_atlas));
+
+    check!(gl::bind_vertex_array(self.vao));
+    check!(gl::bind_buffer(gl::ARRAY_BUFFER, self.vbo));
+    check!(gl::enable_vertex_attrib_array(0));
+    check!(gl::vertex_attrib_pointer_f32(0, 4, false, 0, 0));
+
+    struct Point
+    {
+      x: f32, y: f32,
+      u: f32, v: f32
+    }
+    impl Point
+    {
+      #[inline(always)]
+      pub fn new(nx: f32, ny: f32, nu: f32, nv: f32) -> Point
+      { Point { x: nx, y: ny, u: nu, v: nv } }
+    }
+    let mut coords = vec::with_capacity::<Point>(text.len());
+
+    let mut temp_pos = pos;
+    temp_pos.y += font.height as f32;
+
+    let mut count = 0;
+    for text.each |curr|
+    {
+      let glyph = match font.glyphs.find(&curr)
+      {
+        Some(g) => g,
+        None => fail!(fmt!("Invalid char (%?) in font %? len %?", curr, font.file, font.glyphs.len()))
+      };
+
+      let end_x = temp_pos.x + glyph.offset.x;
+      let end_y = -temp_pos.y - (glyph.dimensions.y - glyph.offset.y);
+      let end_w = glyph.dimensions.x; /* TODO: Use this everywhere. */
+      let end_h = glyph.dimensions.y;
+
+      temp_pos.x += glyph.advance.x; 
+      temp_pos.y += glyph.advance.y; 
+
+      /* Skip empty glyphs. */
+      if end_w <= 0.1 || end_h <= 0.1
+      { loop; }
+
+      coords.push(Point::new(end_x, -end_y - end_h, glyph.tex.x, glyph.tex.y));
+      coords.push(Point::new(end_x, -end_y, glyph.tex.x, glyph.tex.y + (glyph.dimensions.y / (font.atlas_dimensions.y as f32))));
+      coords.push(Point::new(end_x + end_w, -end_y, glyph.tex.x + (glyph.dimensions.x / (font.atlas_dimensions.x as f32)), glyph.tex.y + (glyph.dimensions.y / (font.atlas_dimensions.y as f32))));
+      coords.push(Point::new(end_x, -end_y - end_h, glyph.tex.x, glyph.tex.y));
+      coords.push(Point::new(end_x + end_w, -end_y, glyph.tex.x + (glyph.dimensions.x / (font.atlas_dimensions.x as f32)), glyph.tex.y + (glyph.dimensions.y / (font.atlas_dimensions.y as f32))));
+      coords.push(Point::new(end_x + end_w, -end_y - end_h, glyph.tex.x + (glyph.dimensions.x / (font.atlas_dimensions.x as f32)), glyph.tex.y));
+      count += 6;
+    }
+
+    check!(gl::buffer_data(gl::ARRAY_BUFFER, coords, gl::STREAM_DRAW)); 
+    check!(gl::draw_arrays(gl::TRIANGLES, 0, count));
+
+    check!(gl::disable_vertex_attrib_array(0));
+    check!(gl::bind_buffer(gl::ARRAY_BUFFER, 0));
+    check!(gl::bind_vertex_array(0));
+  }
+}
+
