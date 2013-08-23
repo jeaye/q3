b1ec93c72d80818c536625ee40dc7108620d16c2
jeaye <jeaye@arrownext.com>
2013-08-23 14:42:16 -0700

    #38 Remove @mut from Log

--- src/util/log.rs
+++ src/util/log.rs
@@ -33,7 +33,7 @@ static Verbosity_Error: Verbosity = 1;
 static Verbosity_Info: Verbosity = 2;
 static Verbosity_Debug: Verbosity = 3;
 
-static tls_key: local_data::Key<@mut Log> = &local_data::Key;
+static tls_key: local_data::Key<Log> = &local_data::Key;
 
 #[macro_escape]
 mod log_macros;
@@ -49,7 +49,7 @@ impl Log
 {
   pub fn initialize()
   {
-    let logger = @mut Log
+    let logger = Log
     {
       verbosity: match os::getenv("Q3_LOG").take()
                  {
@@ -72,37 +72,37 @@ impl Log
     log_debug!("Logging system initialized");
   }
 
-  fn get() -> @mut Log
-  {
-    local_data::get(tls_key, 
+  pub fn get_mut<T>(handler: &fn(&mut Log) -> T) -> T
+  { 
+    local_data::get_mut(tls_key, 
     |opt|
     {
       match opt
       {
-        Some(x) => *x,
-        None => fail!("Singleton not available")
+        Some(x) => handler(&mut *x),
+        None => fail!("Singleton not available: Log")
       }
     })
   }
 
   pub fn debug(module: &str, message: &str)
   {
-    let logger = Log::get();
-    if logger.verbosity >= Verbosity_Debug
+    let verbosity = do Log::get_mut |log| { log.verbosity };
+    if verbosity >= Verbosity_Debug
     { Log::log(module, message, Verbosity_Debug); }
   }
 
   pub fn info(module: &str, message: &str)
   {
-    let logger = Log::get();
-    if logger.verbosity >= Verbosity_Info
+    let verbosity = do Log::get_mut |log| { log.verbosity };
+    if verbosity >= Verbosity_Info
     { Log::log(module, message, Verbosity_Info); }
   }
 
   pub fn error(module: &str, message: &str)
   {
-    let logger = Log::get();
-    if logger.verbosity >= Verbosity_Error
+    let verbosity = do Log::get_mut |log| { log.verbosity };
+    if verbosity >= Verbosity_Error
     { Log::log(module, message, Verbosity_Error); }
   }
 
@@ -114,52 +114,54 @@ impl Log
 
   pub fn push()
   {
-    let logger = Log::get();
-    logger.push_level += 1;
+    do Log::get_mut |logger|
+    { logger.push_level += 1; }
   }
 
   pub fn pop()
   {
-    let logger = Log::get();
-
-    assert!(logger.push_level > 0);
-    logger.push_level -= 1;
+    do Log::get_mut |logger|
+    {
+      assert!(logger.push_level > 0);
+      logger.push_level -= 1;
+    }
   }
 
   fn log(module: &str, message: &str, verbosity: Verbosity)
   {
-    let logger = Log::get();
-
-    /* Display the current module. */
-    logger.terminal.fg(color::BRIGHT_WHITE);
-    print(module); 
-    logger.terminal.reset();
-
-    /* Indent as per the push level. */
-    for _ in range(0, logger.push_level)
-    { print("  "); }
-
-    match verbosity
+    do Log::get_mut |logger|
     {
-      Verbosity_Debug =>
+      /* Display the current module. */
+      logger.terminal.fg(color::BRIGHT_WHITE);
+      print(module); 
+      logger.terminal.reset();
+
+      /* Indent as per the push level. */
+      for _ in range(0, logger.push_level)
+      { print("  "); }
+
+      match verbosity
       {
-        logger.terminal.fg(color::BRIGHT_GREEN);
-        print(" debug => ");
-      },
-      Verbosity_Info =>
-      {
-        logger.terminal.fg(color::BRIGHT_YELLOW);
-        print(" info => ");
-      },
-      Verbosity_Error =>
-      {
-        logger.terminal.fg(color::BRIGHT_RED);
-        print(" error => ");
-      },
-      val => fail!("Invalid verbosity for logging: %?", val)
+        Verbosity_Debug =>
+        {
+          logger.terminal.fg(color::BRIGHT_GREEN);
+          print(" debug => ");
+        },
+        Verbosity_Info =>
+        {
+          logger.terminal.fg(color::BRIGHT_YELLOW);
+          print(" info => ");
+        },
+        Verbosity_Error =>
+        {
+          logger.terminal.fg(color::BRIGHT_RED);
+          print(" error => ");
+        },
+        val => fail!("Invalid verbosity for logging: %?", val)
+      }
+      logger.terminal.reset();
+      println(message);
     }
-    logger.terminal.reset();
-    println(message);
   }
 }
 
