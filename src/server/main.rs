6219e808387245a8d9d44762c0546fb91272c70f
jeaye <jeaye@arrownext.com>
2013-10-13 01:11:03 -0700

    #50 Added commandline options

--- src/server/main.rs
+++ src/server/main.rs
@@ -14,6 +14,8 @@
 
 extern mod ui;
 
+use std::os;
+
 struct Server
 {
   log: ~str,
@@ -24,13 +26,40 @@ impl Server
 {
   pub fn new() -> Server
   {
+    let args = os::args();
+    if args.contains(&~"--help")
+    { Server::show_help(); }
+
+    /* Determine the UI driver. */
+    let driver =
+    if args.contains(&~"--gui")
+    { fail!("GUI mode is not yet implemented"); }
+    else
+    { ui::term::initialize().expect("Unable to create terminal UI") };
+
     let server = Server
     {
       log: ~"",
-      ui_driver: ui::term::initialize().expect("Unable to create UI"),
+      ui_driver: driver,
     };
 
     server
+  }
+
+  fn show_help()
+  {
+    let args = os::args();
+
+    println!("Q³ Server {}.{}", env!("VERSION"), env!("COMMIT"));
+    println!("Usage:\n\t{} [options...]", args[0]);
+    println!("");
+    println!("Options:");
+    println!("\t--help\tShows this help menu and exits");
+    println!("\t--tui\tEnable textual user interface mode (default)");
+    println!("\t--gui\tEnable graphical user interface mode");
+    println!("");
+
+    fail!("Exiting");
   }
 }
 
